49ed5125d89f6e2e97deb55f8a14bf91444f3b62
amiltonbandeira <amiltonbraulioadaobandeira@gmail.com>
2025-08-03 13:08:03 +0100

    [NEW] begginer exercises

---
+++ exercises/ex003/meet_name.py
@@ -0,0 +1,3 @@
+name = input("What is your name? \n").upper()
+print("Nice to meet you {}".format(name))
+#input is used to receive values from the keyboard
---
+++ exercises/ex005/number_sum.py
@@ -0,0 +1,4 @@
+number1 = int(input("Type a number : \n"))
+number2 = int(input("Type another number : \n"))
+sum = number1 + number2
+print("{} + {} = {}".format(number1,number2,sum))
