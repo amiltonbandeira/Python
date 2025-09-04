f12a06d7f4941fde04a60f02156d1209d63ce383
amiltonbandeira <amiltonbraulioadaobandeira@gmail.com>
2025-09-04 23:24:48 +0100

    [NEW] creating python lessons , started creating the first python lesson

---
+++ python_user_guide/Python_basics/Lessons/lesson_1.py
@@ -0,0 +1,5 @@
+print("Hello World!")
+#print is a function in python , all function we execute it calling it,
+# Call a functions , is the same as execute the function
+# function as print are called built in functions are like out of the box functions of a language
+#Python has some built in functions.
